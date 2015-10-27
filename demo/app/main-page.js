2031aae83900e173e80b78c316adbd50f78693f2
Vladimir Enchev <vladimir.enchev@telerik.com>
2015-10-27 13:57:05 +0200

    demo app added

---
+++ demo/app/main-page.js
@@ -0,0 +1,13 @@
+var image = require("ui/image");
+var plugin = require("nativescript-screenshot");
+
+var stackLayout;
+exports.stackLoaded = function(args){
+  stackLayout = args.object
+}
+
+exports.buttonTap = function(args){
+  var img = new image.Image();
+  img.imageSource = plugin.getImage(args.object);
+  stackLayout.addChild(img);
+}
