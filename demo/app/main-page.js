bf11639055d918253876646f18103317fafe5f23
Emil Tabakov <emil.tabakov@gmail.com>
2019-08-09 11:51:19 +0300

    chore: update demo app to {N} 6.0

--- demo/app/main-page.js
+++ demo/app/main-page.js
@@ -2,12 +2,12 @@ var image = require("ui/image");
 var plugin = require("nativescript-screenshot");
 
 var stackLayout;
-exports.stackLoaded = function(args){
-  stackLayout = args.object
+exports.stackLoaded = function(args) {
+    stackLayout = args.object
 }
 
-exports.buttonTap = function(args){
-  var img = new image.Image();
-  img.imageSource = plugin.getImage(args.object);
-  stackLayout.addChild(img);
+exports.buttonTap = function(args) {
+    var img = new image.Image();
+    img.imageSource = plugin.getImage(args.object);
+    stackLayout.addChild(img);
 }
